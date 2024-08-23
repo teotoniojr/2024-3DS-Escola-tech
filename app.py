42e55effc81ba62b4ce48bb0dcbeaf2593e4f0e0
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2024-08-23 07:44:33 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+print("""
+
+░█▀▀▀ █▀▀ █▀▀ █▀▀█ █── █▀▀█ 　 ▀▀█▀▀ █▀▀ █▀▀ █──█ 
+░█▀▀▀ ▀▀█ █── █──█ █── █▄▄█ 　 ─░█── █▀▀ █── █▀▀█ 
+░█▄▄▄ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀▀▀ ▀──▀ 　 ─░█── ▀▀▀ ▀▀▀ ▀──▀
+      
+""");
+
+print("1. Cadastrar Aluno")
+print("2. Listar Alunos")
+print("3. Ativar matricula")
+print("4. Sair")
+
+opcao_escolhida = input("Escolha uma opção:")
+print("Você escolheu a opção: ", opcao_escolhida)
